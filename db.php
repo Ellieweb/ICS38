9c352d1892bcaf384b748da216e78fccb603f0d6
Ellieweb <tudtudleanne@gmail.com>
2026-05-02 00:19:20 +0800

    Just connected the DB

--- db.php
+++ db.php
@@ -1,16 +1,16 @@
 <?php
-// $servername = "localhost";
-// $username = "root"; // Default XAMPP username
-// $password = "";     // Default XAMPP password is empty
-// $dbname = "student_resource_hub"; // Change this to your actual DB name
+$servername = "localhost";
+$username = "root"; // Default XAMPP username
+$password = "";     // Default XAMPP password is empty
+$dbname = "student_resource_hub"; // Change this to your actual DB name
 
-// // 1. Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
+// 1. Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// // 2. Check connection
-// if ($conn->connect_error) {
-//     // This triggers if it FAILS
-//     die("Connection failed: " . $conn->connect_error);
-// } 
+// 2. Check connection
+if ($conn->connect_error) {
+    // This triggers if it FAILS
+    die("Connection failed: " . $conn->connect_error);
+} 
 
 ?>
