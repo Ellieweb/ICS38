db3ef515456b13eb5e51b174e0e6a4128c19e231
Ellieweb <tudtudleanne@gmail.com>
2026-05-03 21:28:48 +0800

    edited add resource

--- delete_resource.php
+++ delete_resource.php
@@ -1,20 +1,20 @@
-<!-- <?php
-session_start();
-include('db.php');
+<?php
+// session_start();
+// include('db.php');
 
-if (!isset($_SESSION['user_id'])) { header("Location: login.php"); exit(); }
+// if (!isset($_SESSION['user_id'])) { header("Location: login.php"); exit(); }
 
-if (isset($_GET['id'])) {
-    $id = (int)$_GET['id'];
-    $uid = $_SESSION['user_id']; // The ID of the logged-in user
+// if (isset($_GET['id'])) {
+//     $id = (int)$_GET['id'];
+//     $uid = $_SESSION['user_id']; // The ID of the logged-in user
 
-    // Only delete if the resource ID belongs to the current user
-    $sql = "DELETE FROM resources WHERE resource_id = $id AND user_id = $uid";
+//     // Only delete if the resource ID belongs to the current user
+//     $sql = "DELETE FROM resources WHERE resource_id = $id AND user_id = $uid";
     
-    if ($conn->query($sql)) {
-        header("Location: dashboard.php?msg=success");
-    } else {
-        header("Location: dashboard.php?msg=error");
-    }
-}
-?> -->
+//     if ($conn->query($sql)) {
+//         header("Location: dashboard.php?msg=success");
+//     } else {
+//         header("Location: dashboard.php?msg=error");
+//     }
+// }
+?>
